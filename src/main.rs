127178db6ec1a371a4d608242f43b84a5a5e57e8
marcos-gamarra <marcos.gamarra12345@gmail.com>
2023-05-18 01:37:02 +0800

    .

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,63 @@
-use std::fs::{self, File};
 use std::io::prelude::*;
 
 fn main() {
-    let mut output = File::create("output.html").unwrap();
-    let mut styles = Vec::new();
-    let mut components = Vec::new();
-    let navbar = fs::read_to_string("components/navbar/navbar.html").unwrap();
-    styles.push(String::from("components/navbar/navbar.css"));
-    styles.push(String::from("output.css"));
-    components.push(navbar);
-    let head = generate_head(String::from("Title"), styles);
-    let body = generate_body(components);
-
-    output.write_all(head.as_bytes()).unwrap();
-    output.write_all(body.as_bytes()).unwrap();
+    let menu_id = "test_menu".to_string();
+    let categories = vec!["one".to_string(), "two".to_string(), "three".to_string()];
+    let title = "Test Menu".to_string();
+    generate_menu(menu_id, categories, title);
 }
 
-fn add_style(style_path: &str) -> String {
-    let link = format!("<link rel=\"stylesheet\" href=\"{}\">", style_path);
-    link
+fn generate_menu(menu_id: String, categories: Vec<String>, title: String) {
+    if let Err(e) = std::fs::create_dir_all("menus/".to_string() + &menu_id) {
+        println!("Error creating directory: {}", e);
+    }
+
+    generate_categories_files(categories, menu_id, title);
 }
 
-fn generate_head(title: String, styles: Vec<String>) -> String {
-    let mut head = String::from("<head>");
-    let title = format!("<title>{}</title>", title);
-    let styles = styles
-        .iter()
-        .map(|style| add_style(style))
-        .collect::<Vec<String>>();
-
-    let styles = styles.join("");
-
-    head = head + &title + &styles + "</head>";
-
-    head
+fn generate_categories_files(categories: Vec<String>, menu_id: String, title: String) {
+    let navbar = generate_navbar(&categories);
+    let head = generate_head(&title);
+    let content = "content".to_string();
+    for category in categories {
+        if let Ok(mut file) = std::fs::File::create(format!("menus/{menu_id}/{category}.html")) {
+            let page = generate_page(&navbar, &head, &content);
+            file.write_all(page.as_bytes()).unwrap();
+        }
+    }
 }
 
-fn generate_body(components: Vec<String>) -> String {
-    let mut body = String::from("<body>");
-    let components = components.join("");
-    body = body + &components + "</body>";
-    body
+fn generate_head(title: &str) -> String {
+    format!(
+        "<head>
+            <title>{title}</title>
+            <link rel=\"stylesheet\" href=\"../../style.css\">
+        </head>"
+    )
+}
+
+fn generate_navbar(categories: &Vec<String>) -> String {
+    let mut navbar = "<nav class=\"navbar\">\n".to_string();
+    for category in categories {
+        navbar.push_str(&generate_link(category));
+    }
+    navbar.push_str("</nav>");
+    navbar
+}
+
+fn generate_link(category: &str) -> String {
+    format!("<a href=\"{category}.html\"><button class=\"nav-button\">{category}</button></a>")
+}
+
+fn generate_page(navbar: &str, head: &str, content: &str) -> String {
+    format!(
+        "<!DOCTYPE html>
+        <html lang=\"en\">
+        {head}
+        <body>
+            {navbar}
+            {content}
+        </body>
+        </html>"
+    )
 }
